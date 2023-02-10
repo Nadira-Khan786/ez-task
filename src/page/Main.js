9e9a0dc161243d10cfc3d323d6b95b2beb8a8bd1
Nadira-Khan786 <nadira.khan.sdbg@gmail.com>
2023-02-11 00:06:05 +0530

    Basic setup

---
+++ src/page/Main.js
@@ -0,0 +1,146 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import profileIcon from "../img/Profile.png";
+import "./Main.css";
+const drawerWidth = 340;
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const Main = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar className="header-ctn">
+        <div className="contain">
+          {" "}
+          <div className="title">Client</div>
+          <div className="sub-title">Creation Time</div>
+        </div>
+        <AddBoxIcon className="right-btn" />
+      </Toolbar>
+      <Divider />
+      <div className="card-list">
+        {arr.map((item) => {
+          return (
+            <div className="card">
+              <div className="card-ctn">
+                <div className="label">Company Name</div>
+                <div className="label">Code</div>
+              </div>
+              <div className="card-ctn">
+                <div className="label1">8 Entities</div>
+                <div className="label1">64 User</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <Divider />
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar className="header-ctn">
+          <Typography variant="h6" noWrap component="div">
+            Z
+          </Typography>
+          <div>
+            <img src={profileIcon} />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Typography paragraph>
+          <div className="header">
+            <div className="header-title">Lucas Films Private Limited</div>
+            <div className="edit-btn">Edit</div>
+          </div>
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+export default Main;
